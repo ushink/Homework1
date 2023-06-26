8216bbf16c0c93cd5acc7f059b42f03c77f5ba90
Ulyana <ushinkevich@mail.ru>
2023-06-26 15:31:45 +0400

    удалить студента

---
+++ delete-student.js
@@ -0,0 +1,47 @@
+const listElement = document.getElementById("list");
+
+const students = [
+  {
+    name: "Глеб",
+    color: "#ff2600",
+  },
+];
+
+const renderStudents = () => {
+  const studentsHtml = students
+    .map((student, index) => {
+      return `
+      <li class="student" data-color="${student.color}">
+        <p class="student-name">
+          ${student.name}
+        </p>
+        <p>Любимый цвет: ${student.color} <span style="
+          display:inline-block;
+          width: 15px;
+          height: 15px;
+          background-color: ${student.color};">
+        </span></p>
+        <button data-index="${index}" class="button delete-button">Удалить</button>
+      </li>`;
+    })
+    .join("");
+
+  listElement.innerHTML = studentsHtml;
+
+  const deleteButtons = document.querySelectorAll(".delete-button");
+
+
+  for (const deleteButton of deleteButtons) {
+    deleteButton.addEventListener("click", (event) => {
+      event.stopPropagation();
+
+      const index = deleteButton.dataset.index;
+
+
+      students.splice(index, 1);
+      renderStudents();
+    });
+  }
+};
+
+renderStudents();
